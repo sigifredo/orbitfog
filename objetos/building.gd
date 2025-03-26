b2821365bd39df1aba9ac99b087c72ec96076d89
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2025-03-25 22:34:38 -0500

    Se implementa el cambio de fov al correr.

--- objetos/building.gd
+++ objetos/building.gd
@@ -1,6 +1,7 @@
 extends StaticBody3D
 class_name Building
 
+
 @export_range(0.0, 10.0, 0.1)
 var color_change_speed: float = 0.05
 
@@ -9,17 +10,25 @@ var shared_material: StandardMaterial3D = null
 var time:float = 0.0
 
 
+func _create_material() -> StandardMaterial3D:
+	var material = StandardMaterial3D.new()
+
+	material.emission_enabled = true
+	material.emission_energy_multiplier = 10
+	material.emission = Color(0.65, 0.33, 1.0)
+
+	return material
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var mesh:MeshInstance3D = get_node("neon01")
+	var neon_parent:Node3D = get_node('neon')
 
-	if mesh:
-		var material = mesh.get_active_material(0)
+	if neon_parent:
+		shared_material = _create_material()
 
-		if material is StandardMaterial3D:
-			shared_material = material
-			shared_material.emission_enabled = true
-
+		for neon in neon_parent.get_children():
+			neon.material_override = shared_material
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -27,4 +36,9 @@ func _process(delta: float) -> void:
 
 	if shared_material:
 		var cc:Color = shared_material.emission
-		shared_material.emission = Color.from_hsv(fmod(time * color_change_speed, 1.0), cc.s, cc.v)
+
+		shared_material.emission = Color.from_hsv(
+			fmod(time * color_change_speed + position.x + position.y + position.x, 1.0),
+			cc.s,
+			cc.v
+		)
